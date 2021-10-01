75603251d33c3d9172b535caf9ca42bc9952603b
stancato4u <stancato4u@iutm-inf-f36-12.iutmetz.site.univ-lorraine.fr>
2021-10-01 11:50:34 +0200

    non

---
+++ src/cpoa_td1/Periodicite.java
@@ -0,0 +1,41 @@
+package cpoa_td1;
+
+public class Periodicite {
+private int id_periodicite;
+private String libelle;
+
+public Periodicite(String libelle) {
+    this(-1, libelle);
+}
+public Periodicite(int id_periodicite, String libelle) {
+this.setId_periodicite(id_periodicite);
+this.setLibelle(libelle);
+}
+
+public int getId_periodicite() 
+{ 
+return this.id_periodicite; 
+}
+
+public String getLibelle() 
+{ 
+return this.libelle; 
+}
+
+public void setId_periodicite(int id_periodicite) {
+this.id_periodicite = id_periodicite;
+}
+
+public void setLibelle(String libelle) {
+if (libelle==null || libelle.trim().length()==0) {
+throw new IllegalArgumentException("Libelle vide !");
+}
+this.libelle = libelle;
+}
+
+public String toString() {
+return "(" + (this.id_periodicite>=0?this.id_periodicite:"nouveau") + ") " + 
+this.libelle;
+}
+
+}
