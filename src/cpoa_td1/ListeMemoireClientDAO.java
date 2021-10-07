9b0d8032e4257bd7541d20b72f046e6b5d592692
Made-In-Heaven-D4C-Love-Train <90892596+Made-In-Heaven-D4C-Love-Train@users.noreply.github.com>
2021-10-07 17:04:34 +0200

    Add files via upload

---
+++ src/cpoa_td1/ListeMemoireClientDAO.java
@@ -0,0 +1,94 @@
+package cpoa_td1;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListeMemoireClientDAO implements ClientDAO {
+
+	private static ListeMemoireClientDAO instance;
+
+	private List<Client> donnees;
+
+
+	public static ListeMemoireClientDAO getInstance() {
+
+		if (instance == null) {
+			instance = new ListeMemoireClientDAO();
+		}
+
+		return instance;
+	}
+
+	private ListeMemoireClientDAO() {
+
+		this.donnees = new ArrayList<Client>();
+
+		this.donnees.add(new Client(1, 97, 757, "Mensuel", "non", "non", "non", "non"));
+		this.donnees.add(new Client(2, 78, 999, "Quotidien", "oui", "oui", "oui", "oui"));
+	}
+
+
+	@Override
+	public boolean create(Client objet) {
+
+		objet.setId_client(3);
+		// Ne fonctionne que si l'objet métier est bien fait...
+		while (this.donnees.contains(objet)) {
+
+			objet.setId_client(objet.getId_client() + 1);
+		}
+		boolean ok = this.donnees.add(objet);
+		
+		return ok;
+	}
+
+	@Override
+	public boolean update(Client objet) {
+		
+		// Ne fonctionne que si l'objet métier est bien fait...
+		int idx = this.donnees.indexOf(objet);
+		if (idx == -1) {
+			throw new IllegalArgumentException("Tentative de modification d'un objet inexistant");
+		} else {
+			
+			this.donnees.set(idx, objet);
+		}
+		
+		return true;
+	}
+
+	@Override
+	public boolean delete(Client objet) {
+
+		Client supprime;
+		
+		// Ne fonctionne que si l'objet métier est bien fait...
+		int idx = this.donnees.indexOf(objet);
+		if (idx == -1) {
+			throw new IllegalArgumentException("Tentative de suppression d'un objet inexistant");
+		} else {
+			supprime = this.donnees.remove(idx);
+		}
+		
+		return objet.equals(supprime);
+	}
+
+	@Override
+	public Client getById(int id, int id1, int id2) {
+		// Ne fonctionne que si l'objet métier est bien fait...
+		int idx = this.donnees.indexOf(new Client(id, id1, id2, "test", "test", "test", "test", "test"));
+		if (idx == -1) {
+			throw new IllegalArgumentException("Aucun objet ne possède cet identifiant");
+		} else {
+			return this.donnees.get(idx);
+		}
+	}
+
+	@Override
+	public ArrayList<Client> findAll() {
+		return (ArrayList<Client>) this.donnees;
+	}
+}
