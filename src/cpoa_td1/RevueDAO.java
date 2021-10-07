c5e642a0d34bebbf106f9b44bfea91bba86cb2a3
Made-In-Heaven-D4C-Love-Train <90892596+Made-In-Heaven-D4C-Love-Train@users.noreply.github.com>
2021-10-07 16:49:13 +0200

    Add files via upload

---
+++ src/cpoa_td1/RevueDAO.java
@@ -0,0 +1,17 @@
+package cpoa_td1;
+
+import java.util.ArrayList;
+
+public interface RevueDAO {
+
+	boolean create(Revue objet);
+
+	boolean update(Revue objet);
+
+	boolean delete(Revue objet);
+
+	ArrayList<Revue> findAll();
+
+	Revue getById(int id);
+
+}
