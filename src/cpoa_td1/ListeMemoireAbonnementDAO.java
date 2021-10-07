6e952196eae3f30cb61bb56b98f28edd2cd4dee8
Made-In-Heaven-D4C-Love-Train <90892596+Made-In-Heaven-D4C-Love-Train@users.noreply.github.com>
2021-10-07 17:16:25 +0200

    Add files via upload

---
+++ src/cpoa_td1/ListeMemoireAbonnementDAO.java
@@ -0,0 +1,94 @@
+package cpoa_td1;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListeMemoireAbonnementDAO implements AbonnementDAO {
+
+	private static ListeMemoireAbonnementDAO instance;
+
+	private List<Abonnement> donnees;
+
+
+	public static ListeMemoireAbonnementDAO getInstance() {
+
+		if (instance == null) {
+			instance = new ListeMemoireAbonnementDAO();
+		}
+
+		return instance;
+	}
+
+	private ListeMemoireAbonnementDAO() {
+
+		this.donnees = new ArrayList<Abonnement>();
+
+		this.donnees.add(new Abonnement(0, 0, 0, null, null, null, null));
+		this.donnees.add(new Abonnement(0, 0, 0, null, null, null, null));
+	}
+
+
+	@Override
+	public boolean create(Abonnement objet) {
+
+		objet.setId_client(3);
+		// Ne fonctionne que si l'objet métier est bien fait...
+		while (this.donnees.contains(objet)) {
+
+			objet.setId_abonnement(objet.getId_abonnement() + 1);
+		}
+		boolean ok = this.donnees.add(objet);
+		
+		return ok;
+	}
+
+	@Override
+	public boolean update(Abonnement objet) {
+		
+		// Ne fonctionne que si l'objet métier est bien fait...
+		int idx = this.donnees.indexOf(objet);
+		if (idx == -1) {
+			throw new IllegalArgumentException("Tentative de modification d'un objet inexistant");
+		} else {
+			
+			this.donnees.set(idx, objet);
+		}
+		
+		return true;
+	}
+
+	@Override
+	public boolean delete(Abonnement objet) {
+
+		Abonnement supprime;
+		
+		// Ne fonctionne que si l'objet métier est bien fait...
+		int idx = this.donnees.indexOf(objet);
+		if (idx == -1) {
+			throw new IllegalArgumentException("Tentative de suppression d'un objet inexistant");
+		} else {
+			supprime = this.donnees.remove(idx);
+		}
+		
+		return objet.equals(supprime);
+	}
+
+	@Override
+	public Abonnement getById(int id, int id1, int id2) {
+		// Ne fonctionne que si l'objet métier est bien fait...
+		int idx = this.donnees.indexOf(new Abonnement(id, id1, id2, null, null, null, null));
+		if (idx == -1) {
+			throw new IllegalArgumentException("Aucun objet ne possède cet identifiant");
+		} else {
+			return this.donnees.get(idx);
+		}
+	}
+
+	@Override
+	public ArrayList<Abonnement> findAll() {
+		return (ArrayList<Abonnement>) this.donnees;
+	}
+}
